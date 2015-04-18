8351ca8b18b1c416e79d702404606f15fc769345
inventory <salman.saleemkma@gmail.com>
2015-04-18 16:48:38 +0000

    new order save issue solved

--- app/controllers/bookings/new.js
+++ app/controllers/bookings/new.js
@@ -24,7 +24,7 @@ export default Ember.Controller.extend({
             if (this.get('isValid')) {
                 var _this = this;
                 this.get('model').save().then(function(booking) {
-                    _this.transitionToRoute('bookings', booking);
+                    _this.transitionToRoute('bookings');
                 });
             } else {
                 this.set('errorMessage', 'You have to fill all the fields');
