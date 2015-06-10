c5a52456fe63dcd9c441ec05e1819e7ca5b83255
salmansaleemkma <salman.saleemkma@gmail.com>
2015-06-10 05:29:00 -0400

    using this.store.all for booking quote

--- app/routes/bookings.js
+++ app/routes/bookings.js
@@ -2,21 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        return this.store.find('booking');
+        return this.store.all('booking');
     },
     actions: {
 	delete: function(booking) {
 	    booking.destroyRecord();
 	    return false;
-	},
-        save: function() {
-            console.log('+--- save action bubbled up to bookings route');
-            return false;
-        },
-        cancel: function() {
-            console.log('+--- cancel action bubbled up to bookings route');
-            return false;
-        }
+	}
     }
     
 });
