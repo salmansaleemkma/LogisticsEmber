8c0067ef153bfe8f29fc01c042139411eca18b2d
salmansaleemkma <salman.saleemkma@gmail.com>
2015-06-11 05:24:36 -0400

    clearing things up

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,20 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
 	return this.store.find('booking');
-    },
-    actions: {
-	showModal: function(name, model) {
-	    this.render(name, {
-		into: 'application',
-		outlet: 'modal',
-		model: model
-	    });
-	},
-	removeModal: function() {
-	    this.disconnectOutlet({
-		outlet: 'modal',
-		parentView: 'application'
-	    });
-	}
     }
 });
